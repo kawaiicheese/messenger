c43b192c903c9eae29ceb8e8335773a5330d9cf5
kawaiicheese <josephlu116@gmail.com>
2021-07-16 09:09:04 -0400

    read bubble

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -3,18 +3,39 @@ import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
-const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+const Messages = ({ messages, otherUser, userId }) => {
+  const sentMessages = messages.filter(
+    (message) => message.senderId === userId
+  );
+  const unreadMessagesCount = sentMessages.filter(
+    (message) => !message.read
+  ).length;
+  const lastReadMessageId =
+    unreadMessagesCount < sentMessages.length
+      ? sentMessages[sentMessages.length - 1 - unreadMessagesCount].id
+      : 0;
 
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
+        const isLastReadMessage = message.id === lastReadMessageId;
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+            isLastReadMessage={isLastReadMessage}
+          />
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          <OtherUserBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+          />
         );
       })}
     </Box>
